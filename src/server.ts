f3c44033c00ad8fdc0ff70ee42813e27b20b034a
leirdan <uouleirdan@gmail.com>
2023-04-14 10:51:04 -0300

    feat: deploy config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { router } from "./router/router";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(router);
@@ -7,6 +9,6 @@ server.use(express.json());
 server.use(express.urlencoded());
 server.set("view engine", "ejs");
 
-server.listen(8082, () => {
+server.listen(process.env.PORT, () => {
 	console.log("it works!");
 });
